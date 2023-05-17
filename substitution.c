86b86bdfc61a3a75056fb6a5449c7a4fe4f438e3
TomasNagy <tomi.nagy@outlook.com>
2023-05-17 20:16:18 +0200

    Add substitution.c

---
+++ substitution.c
@@ -0,0 +1,23 @@
+#include <cs50.h>
+#include <stdio.h>
+
+void encript_text(string plaintext);
+
+int main(int argc, string argv[])
+{
+	// Get CLA from user and check if it has 2 arguments
+
+	// Check the key argument to have 26 alphabetic characters
+
+	// Ask user for a plaintext
+
+	// Convert the plaintext to ciphertext
+
+	// Print ciphertext
+}
+
+// Define function to convert plaintext to ciphertext
+void encript_text(string plaintext)
+{
+
+}
